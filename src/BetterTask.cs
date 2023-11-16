249ee8cf40b8a402dff4d6741e09ec30cd54344c
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2023-11-15 19:14:09 -0500

    Add project files.

---
+++ src/BetterTask.cs
@@ -0,0 +1,17 @@
+﻿namespace BetterTasks
+{
+    public class BetterTask
+    {
+
+        internal Action Action { get; private set; }
+        internal Thread? Thread { private get; set; }
+
+        public BetterTask(Action action)
+        {
+            Action = action;
+
+            TaskScheduler.CreateTask(this);
+        }
+
+    }
+}
---
+++ src/TaskScheduler.cs
@@ -0,0 +1,64 @@
+﻿namespace BetterTasks
+{
+    internal class TaskScheduler
+    {
+
+        private static TaskScheduler? instance;
+
+        private List<Thread> threads;
+
+        private int tasksPerThread = 1;
+
+        private List<BetterTask> waitingTasks;
+
+        public static void CreateTask(BetterTask task)
+        {
+            instance ??= new();
+            instance.CreateThread(task);
+        }
+
+        private TaskScheduler()
+        {
+            threads = new();
+            waitingTasks = new();
+        }
+
+        private void CreateThread(BetterTask task)
+        {
+            waitingTasks.Add(task);
+
+            if (waitingTasks.Count >= tasksPerThread * threads.Count)
+            {
+                Thread thread = new(new ThreadStart(StartThread));
+                thread.Start();
+
+                threads.Add(thread);
+            }
+        }
+
+        private void StartThread()
+        {
+            while (true)
+            {
+                BetterTask task;
+
+                // Lock prevents other threads from accessing an object. If another thread is already accessing the object, it will wait until the lock is released.
+                lock (waitingTasks)
+                {
+                    if (waitingTasks.Count == 0)
+                        break;
+
+                    task = waitingTasks[0];
+                    waitingTasks.RemoveAt(0);
+                }
+
+                task.Thread = Thread.CurrentThread;
+
+                task.Action();
+
+                task.Thread = null;
+            }
+        }
+
+    }
+}
