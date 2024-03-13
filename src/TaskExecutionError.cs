7367965258c1d6f79e17e4c26673e2674d920627
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-03-13 15:29:48 -0400

    Various improvements

---
+++ src/TaskExecutionError.cs
@@ -0,0 +1,19 @@
+﻿namespace BetterTasks;
+
+public class TaskExecutionError : Exception
+{
+
+    public TaskExecutionError() : base("An exception occurred while executing a task.")
+    {
+    }
+
+    public TaskExecutionError(string message) : base(message)
+    {
+    }
+
+    public TaskExecutionError(string message, Exception innerException) : base(
+        message, innerException)
+    {
+    }
+
+}
--- src/TaskScheduler.cs
+++ src/TaskScheduler.cs
@@ -34,74 +34,71 @@ internal class TaskScheduler
 
             lock (availableThreads)
             {
-                if (waitingTasks.Count >= QUEUED_TASKS_PER_AVAILABLE_THREAD * availableThreads.Count)
-                {
-                    Thread thread = new(new ThreadStart(StartThread));
-                    thread.Start();
+                if (waitingTasks.Count < QUEUED_TASKS_PER_AVAILABLE_THREAD *
+                    availableThreads.Count)
+                    return;
 
-                    availableThreads.Add(thread);
-                }
+                Thread thread = new(StartThread);
+                thread.Start();
+
+                availableThreads.Add(thread);
             }
         }
     }
 
     private void StartThread()
     {
-        try
+        while (true)
         {
-            while (true)
+            ITask task;
+
+            // Lock prevents other threads from accessing an object. If another thread is already accessing the object, it will wait until the lock is released.
+            lock (waitingTasks)
             {
-                ITask task;
-
-                // Lock prevents other threads from accessing an object. If another thread is already accessing the object, it will wait until the lock is released.
-                lock (waitingTasks)
+                if (waitingTasks.Count == 0)
                 {
-                    if (waitingTasks.Count == 0)
+                    lock (availableThreads)
                     {
-                        lock (availableThreads)
+                        // Keep one thread running to avoid a delay when starting tasks
+                        if (availableThreads.Count > MIN_THREADS)
                         {
-                            // Keep one thread running to avoid a delay when starting tasks
-                            if (availableThreads.Count > MIN_THREADS)
-                            {
-                                availableThreads.Remove(Thread.CurrentThread);
-                                break;
-                            }
-                            else continue;
+                            availableThreads.Remove(Thread.CurrentThread);
+                            break;
                         }
+                        continue;
                     }
-
-                    task = waitingTasks[0];
-                    waitingTasks.RemoveAt(0);
                 }
 
-                lock (availableThreads)
-                    availableThreads.Remove(Thread.CurrentThread);
-
-                task.Thread = Thread.CurrentThread;
-
-                Thread.CurrentThread.Priority = task.Priority;
-
-                try
-                {
-                    //Console.WriteLine("Executing task on thread " + Thread.CurrentThread.ManagedThreadId);
-                    task.Result = task.Execute();
-                    //Console.WriteLine("Task on thread " + Thread.CurrentThread.ManagedThreadId + " completed");
-                }
-                catch (ThreadInterruptedException) { }
-
-                Thread.CurrentThread.Priority = ThreadPriority.Normal;
-
-                task.Thread = null;
-
-                task.OnActionComplete();
-
-                lock (availableThreads)
-                    availableThreads.Add(Thread.CurrentThread);
+                task = waitingTasks[0];
+                waitingTasks.RemoveAt(0);
             }
-        }
-        catch (Exception e)
-        {
-            throw new Exception("An error occurred while executing a task.", e);
+
+            lock (availableThreads)
+                availableThreads.Remove(Thread.CurrentThread);
+
+            task.Thread = Thread.CurrentThread;
+
+            Thread.CurrentThread.Priority = task.Priority;
+
+            try
+            {
+                task.Result = task.Execute();
+            }
+            catch (ThreadInterruptedException) { }
+            catch (Exception e)
+            {
+                throw new TaskExecutionError(
+                    "An error occurred while executing a task.", e);
+            }
+
+            Thread.CurrentThread.Priority = ThreadPriority.Normal;
+
+            task.Thread = null;
+
+            task.OnActionComplete();
+
+            lock (availableThreads)
+                availableThreads.Add(Thread.CurrentThread);
         }
     }
 
